c188106c1efe52ea87a0b82b10ba1d62275e4a36
siddhisingh353 <117386927+siddhisingh353@users.noreply.github.com>
2023-11-05 20:10:11 +0000

    conversition datatype

--- 01_basicjs/03_conversitionoperation.js
+++ 01_basicjs/03_conversitionoperation.js
@@ -1,11 +1,11 @@
 //*******************conversion in number******************************* */ 
 
-let score = "siddhi"
+// let score = "siddhi"
 
 // console.log(typeof score);
 // console.log(typeof (score));
 
-let valueInNumber = Number(score)
+// let valueInNumber = Number(score)
 
 // console.log(typeof valueInNumber);
 // console.log(valueInNumber);
@@ -17,9 +17,9 @@ let valueInNumber = Number(score)
 // true=1,false=0
 // ************************conversion in boolean**********************************
 
-let isLoggedIn = "siddhi"
+// let isLoggedIn = "siddhi"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 // console.log(booleanIsLoggedIn);
 
@@ -29,10 +29,48 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 // **********************converaion in string************************************
 
-let someNum = 33
-let stringNumber = String(someNum)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNum = 33
+// let stringNumber = String(someNum)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 
+// ******************OPERATIONS***********************
 
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hey"
+let str2 = "siddhi"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1+ "2");
+// console.log("1" + "2");
+// console.log("1"+ 2+ 2);
+// console.log(1+ 2+ "2");
+// in js when string comes first then all trated as string and if string is in last then conversion completes first.
+
+
+// console.log(+true);  +true is acceptable like true 
+// console.log(true+);  error
+
+// console.log(+"");   0 <=o/p
+
+let num1, num2, num3
+num1 = numu2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+// prefix&postfix
