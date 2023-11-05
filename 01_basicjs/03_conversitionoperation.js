790fb5f87a679b4217faeaa6656fb205f867d40f
siddhisingh353 <117386927+siddhisingh353@users.noreply.github.com>
2023-11-05 19:12:15 +0000

    coversitionof datatype

--- 01_basicjs/03_conversitionoperation.js
+++ 01_basicjs/03_conversitionoperation.js
@@ -1,4 +1,38 @@
-let score = 20
+//*******************conversion in number******************************* */ 
 
-console.log(typeof score);
-console.log(typeof (score));
+let score = "siddhi"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// last o/t is NaN(not a num) for string ex.20abc
+
+// "33"=>33
+// "33abc"=>NaN
+// true=1,false=0
+// ************************conversion in boolean**********************************
+
+let isLoggedIn = "siddhi"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+// 1= true; 0= false
+// ""(empty string)= false
+// "siddhi"=true
+
+// **********************converaion in string************************************
+
+let someNum = 33
+let stringNumber = String(someNum)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
