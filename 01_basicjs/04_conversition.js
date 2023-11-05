917e4cc7972cbf39af9487113075551673691d03
siddhisingh353 <117386927+siddhisingh353@users.noreply.github.com>
2023-11-05 20:27:37 +0000

    conversion

---
+++ 01_basicjs/04_conversition.js
@@ -0,0 +1,15 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2!=1);
+
+console.log("2">1);
+console.log("02">1);
+// js automatically convert it into num
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+console.log(null>0);
+
