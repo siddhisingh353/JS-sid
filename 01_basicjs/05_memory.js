b90b2a67f4274abef6b914c66d2a60df7bdc4f85
siddhisingh353 <117386927+siddhisingh353@users.noreply.github.com>
2023-11-06 12:43:14 +0000

    memory

--- 01_basicjs/05_memory.js
+++ 01_basicjs/05_memory.js
@@ -1,1 +1,20 @@
+// *****************memories 2 types************************
+// 1.Stack(primitive : we got copy of variable what we declare) primitive comes in stack kind of memory
+// 2.Heap(non_premitive : refrence of original value,change happend in original value)
 
+let myYoutubeName = "siddhi.com"
+
+let anotherName =  myYoutubeName
+
+anotherName = "chaicode"
+
+console.log(myYoutubeName);
+console.log(anotherName);
+
+
+let userOne = {
+email: "siddhi@gmail.com"
+upi: "user@ybl"
+}
+
+ let userTwo = userOne
