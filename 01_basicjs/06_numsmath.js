ba7b59b96d08fb1f4b8d80b70500dc5d0106727b
siddhisingh353 <117386927+siddhisingh353@users.noreply.github.com>
2023-11-08 18:22:56 +0000

    mathsum

--- 01_basicjs/06_numsmath.js
+++ 01_basicjs/06_numsmath.js
@@ -1,1 +1,9 @@
-const 
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance)
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+console.log(typeof (balance));
